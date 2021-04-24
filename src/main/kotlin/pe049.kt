a5c6d1c8891adb9472d3fe60bef887804a7759d4
marckoch <mako.java@posteo.de>
2021-04-24 13:31:42 +0200

    add solution for 050

---
+++ src/main/kotlin/pe049.kt
@@ -0,0 +1,33 @@
+import util.primes
+
+fun main() {
+    primesWith4digits
+        .filter { isFirstOfTriplet(it) }
+}
+
+fun isFirstOfTriplet(first: Long): Boolean {
+    val limit = (10_000 - first) / 2   // because first + diff + diff must not exceed 10_000
+
+    // 2 because primes have always a difference of at least 2
+    for (diff in 2..limit step 2) {
+        val second = first + diff
+        val third = second + diff
+        if (primesWith4digits.contains(second) && primesWith4digits.contains(third) &&
+            isPandigitalPair(first, second) && isPandigitalPair(second, third)
+        ) {
+            println("$first$second$third  (diff=$diff)")
+            return true
+        }
+    }
+    return false
+}
+
+fun isPandigitalPair(p1: Long, p2: Long): Boolean {
+    return p1.toString().toCharArray().sorted() == p2.toString().toCharArray().sorted()
+}
+
+val primesWith4digits =
+    primes()
+        .dropWhile { it < 1_000 }
+        .takeWhile { it < 10_000 }
+        .toList()
