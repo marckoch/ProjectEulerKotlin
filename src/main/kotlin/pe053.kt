68820c144204027636675073aba8ea3c5e6f2ac6
marckoch <mako.java@posteo.de>
2021-04-25 11:41:26 +0200

    add solution for 053

---
+++ src/main/kotlin/pe053.kt
@@ -0,0 +1,29 @@
+import util.factorial
+import java.math.BigInteger
+
+// https://projecteuler.net/problem=53
+fun main() {
+    val limit = BigInteger.valueOf(1_000_000L)
+
+    var sum = 0
+    for (n in 1..100) {
+        for (r in 0..n) {
+            if (comb(n, r) >= limit)
+                sum++
+        }
+    }
+    println(sum)
+
+    (1..100).flatMap { n ->
+        (0..n).map { r ->
+            comb(n, r)
+        }
+    }
+        .filter { it > limit }
+        .count()
+        .let { println(it) }
+}
+
+fun comb(n: Int, r: Int): BigInteger {
+    return factorial(n) / (factorial(r) * factorial(n - r))
+}
