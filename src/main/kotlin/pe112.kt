3d35c34a1b5a1e5921770242e2c3e3ca6f520dce
marckoch <mako.java@posteo.de>
2021-05-24 11:29:30 +0200

    add solution for 112

---
+++ src/main/kotlin/pe112.kt
@@ -0,0 +1,99 @@
+package pe112
+
+import kotlin.system.measureTimeMillis
+
+// https://projecteuler.net/problem=112
+fun main() {
+    val time = measureTimeMillis { solveProjectEuler() }
+    println(time)
+}
+
+fun solveProjectEuler() {
+    val expectedRatio = 0.99
+    var countInc = 0
+    var countDec = 0
+    var countDoubles = 0
+    var countBouncy = 0
+    var found = false
+    var n = 1L
+    while (!found) {
+        val hasIncDigits = hasIncreasingDigits(n)
+        if (hasIncDigits) {
+            countInc++
+        }
+
+        val hasDecDigits = hasDecreasingDigits(n)
+        if (hasDecDigits) {
+            countDec++
+        }
+
+        // account for 6666 or 3333333, those will be counted twice
+        if (hasDecDigits && hasIncDigits) {
+            countDoubles++
+        }
+
+        if (!hasDecDigits && !hasIncDigits) {
+            countBouncy++
+        }
+
+        if (countBouncy >= expectedRatio * n) {
+            val ratioHitAt = n.toInt()
+            println(String.format("ratio of %s hit at n=%s", expectedRatio, ratioHitAt))
+            found = true
+        } else {
+            n++
+        }
+    }
+    println("inc:    $countInc")
+    println("dec:    $countDec")
+    println("doubles $countDoubles")
+    println("nonbouncy:    ${countInc + countDec - countDoubles}")
+    println("bouncy: $countBouncy")
+    println("n: $n")
+    println("%: ${countBouncy.toDouble() / n}")
+}
+
+private fun hasIncreasingDigits(number: Long): Boolean {
+    // too slow
+//    return digits(number).toList().windowed(2, 1).all { list ->
+//        list.first() <= list.last()
+//    }
+
+    val digits = digits(number)
+    for (i in 0 until digits.size - 1) {
+        if (digits[i] > digits[i + 1] ) return false
+    }
+    return true
+}
+
+private fun hasDecreasingDigits(number: Long): Boolean {
+    // too slow
+//    return digits(number).toList().windowed(2, 1).all { list ->
+//        list.first() >= list.last()
+//    }
+
+    val digits = digits(number)
+    for (i in 0 until digits.size - 1) {
+        if (digits[i] < digits[i + 1]) return false
+    }
+    return true
+}
+
+// we split a Long into its digits
+// e.g. 3241L -> (3,2,4,1)
+private fun digits(number: Long): IntArray {
+    var n = number
+    val digitCount = countDigits(n)
+    val result = IntArray(digitCount)
+    var index = 0
+    while (n > 0) {
+        val lastDigit = n.toInt() % 10
+        result[digitCount - 1 - index++] = lastDigit
+        n /= 10
+    }
+    return result
+}
+
+fun countDigits(number: Long): Int {
+    return number.toString().length
+}
