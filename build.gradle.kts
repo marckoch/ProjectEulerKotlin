95e3092f1ce856e5218aa5b522d8696629a5c8d1
marckoch <mako.java@posteo.de>
2021-06-07 07:06:24 +0200

    add combinatorics

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.github.shiguruikai:combinatoricskt:1.6.0")
+
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
